c0b41755226a79605a26a75bb61e516fb35ea002
LilyanLefevre <lilyandu70.7@gmail.com>
2023-02-07 19:10:16 +0100

    Add infinite scroll to chirp list

--- src/components/ChirpList.vue
+++ src/components/ChirpList.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
 import TweetComponent from "./TweetComponent.vue";
 import {Tweet} from "../classes/Tweet";
+import InfiniteLoading from "v3-infinite-loading";
+import "v3-infinite-loading/lib/style.css";
+import {ref} from "vue"; //required if you're not going to override default slots
+
+let tweets = ref([]);
+let page = 1;
+const load = async ($state: { complete: () => void; loaded: () => void; error: () => void; }) => {
+  console.log("loading...");
+  setTimeout(()=>{
+    for(let i = 0; i <= 5; i++) {
+      tweets.value.push(new Tweet());
+      $state.loaded();
+    }
+  }, 2000)
+
+};
+
 </script>
 
 <template>
-  <TweetComponent :tweet="new Tweet()"></TweetComponent>
-  <TweetComponent :tweet="new Tweet()"></TweetComponent>
-  <TweetComponent :tweet="new Tweet()"></TweetComponent>
-  <TweetComponent :tweet="new Tweet()"></TweetComponent>
-  <TweetComponent :tweet="new Tweet()"></TweetComponent>
-  <TweetComponent :tweet="new Tweet()"></TweetComponent>
-  <TweetComponent :tweet="new Tweet()"></TweetComponent>
-  <TweetComponent :tweet="new Tweet()"></TweetComponent>
+  <div class="result" v-for="tweet in tweets">
+    <TweetComponent :tweet="tweet"></TweetComponent>
+  </div>
+  <InfiniteLoading class="mt-2" @infinite="load" />
 </template>
 
 <style scoped>
-
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,6 +5,11 @@
         <img src="../../public/images/logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
         Chirper
       </a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i>
+        </button>
+      </form>
     </div>
   </nav>
 </template>
