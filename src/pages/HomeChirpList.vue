6b462d57590b7fbde8ccfdbcaae758057d1adf51
LilyanLefevre <lilyandu70.7@gmail.com>
2023-02-18 17:46:12 +0100

    DONE Create a component to display 'Home' and ChirpList with appropriate style

---
+++ src/pages/HomeChirpList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="d-inline-block border-start border-end" style="height: 100vh;">
+    <h1 class="text-start ps-3">Home</h1>
+    <ChirpList class="border-top" style="height: 91.7vh"/>
+  </div>
+</template>
+<script>
+import ChirpList from "../components/ChirpList.vue";
+
+export default {
+  name: 'HomeChirpList',
+  components: {ChirpList}
+}
+</script>
